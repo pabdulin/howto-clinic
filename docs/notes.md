e7cd0b48f536aad485ce38d2de276f72863a3822
Petr Abdulin <pabdulin@gmail.com>
2021-08-10 13:42:54 +0700

    Add mainApp from sample project

--- docs/notes.md
+++ docs/notes.md
@@ -1,4 +1,9 @@
 ## project setup
 1. created empty "Console App" Kt project using IJ IDEA 2021.2 (Gradle Kotlin, JUnit 5, openjdk 16)
 2. added `.gitignore` from sample app (enterprise_dev_course)
+3. added `buildSrc` from final sample acts as a single-source for lib version
+
+## gradle 
+1. use "gradle --stop" to stop running servers for whatever reason
+2. use .\gradlew -q projects to check projects structure seealso: https://docs.gradle.org/current/userguide/multi_project_builds.html
 3. 
--- mainApp/build.gradle.kts
+++ mainApp/build.gradle.kts
@@ -1,29 +1,91 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins {
-    kotlin("jvm") version "1.5.10"
-    application
+buildscript {
+    repositories {
+        jcenter()
+        mavenCentral()
+        mavenLocal()
+    }
+    dependencies {
+        classpath(Libs.kotlin_stdlib)
+        classpath(Libs.kotlin_jdk8)
+        classpath(Libs.kotlin_reflect)
+    }
 }
-
-group = "pabdulin"
-version = "1.0-SNAPSHOT"
 
 repositories {
+    jcenter()
     mavenCentral()
+    mavenLocal()
+    maven(url = "https://dl.bintray.com/konform-kt/konform")
 }
 
+plugins {
+    java
+    kotlin("jvm")
+    id("org.springframework.boot") version "2.3.0.RELEASE"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    kotlin("plugin.spring") version "1.3.72"
+}
+
+subprojects {
+    group = "com.pabdulin.mainApp"
+
+    apply {
+        plugin("java")
+    }
+
+    repositories {
+        jcenter()
+        mavenCentral()
+        mavenLocal()
+        maven(url = "https://dl.bintray.com/konform-kt/konform")
+    }
+
+    tasks {
+        withType<KotlinCompile> {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+        withType<Test> {
+            useJUnitPlatform()
+
+            maxParallelForks = 10
+
+            testLogging {
+                events(TestLogEvent.PASSED, TestLogEvent.FAILED, TestLogEvent.SKIPPED)
+                showStandardStreams = true
+                exceptionFormat = TestExceptionFormat.FULL
+            }
+        }
+    }
+}
 dependencies {
-    testImplementation(kotlin("test"))
+    // kotlin
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // spring
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
 }
 
-tasks.test {
+tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
-
-application {
-    mainClass.set("MainKt")
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
