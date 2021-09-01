c30e77f491fe3ccdbb799c6d347b13fb1100f62c
Petr Abdulin <pabdulin@gmail.com>
2021-09-01 15:10:10 +0700

    Use docker-compose to run/stop etc local env

--- cleanAndRun.sh
+++ cleanAndRun.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-set -e
-
-imageTag=$1
-if [ -z "$1" ]
-  then
-    echo No image tag provided. latest will be used
-    imageTag=latest
-fi
-
-(exec "${BASH_SOURCE%/*}/mainApp/clean.sh" $imageTag)
-(exec "${BASH_SOURCE%/*}/mainApp/run.sh" $imageTag)
+docker-compose down
+./run.sh
--- run.sh
+++ run.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-set -e
-
-imageTag=$1
-if [ -z "$1" ]
-  then
-    echo No image tag provided. latest will be used
-    imageTag=latest
-fi
-
-(exec "${BASH_SOURCE%/*}/mainApp/run.sh" $imageTag)
+docker-compose up -d
